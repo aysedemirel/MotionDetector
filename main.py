90a6af4f5b953048a8829eea128996cf513ffd87
elifnur kaçar <elifnur.kacar6@gmail.com>
2018-04-21 16:02:59 +0300

    additional GUI

--- main.py
+++ main.py
@@ -10,23 +10,29 @@ class MainMenu(Frame):
         video_frame = Frame(root, width=600, height=600)
         video_frame.grid(row=0, column=0, padx=20, pady=10)
 
-        photo = ImageTk.PhotoImage(Image.open("default1.png"))
-        video1 = Label(video_frame, width=600, height=350, image=photo)
+        image7 = Image.open("video-generic.png")
+        photo7 = ImageTk.PhotoImage(image7)
+        video1 = Label(video_frame, width=600, height=350, image=photo7,bg="black")
         video1.grid(row=0, column=0, padx=20, pady=10)
-        video1.image = photo
+        video1.image = photo7
 
-        video2 = Label(video_frame, width=600, height=350, image=photo)
+        video2 = Label(video_frame, width=600, height=350, image=photo7,bg="black")
         video2.grid(row=1, column=0, padx=20, pady=10)
-        video2.image = photo
+        video2.image = photo7
 
         rightFrame = Frame(root, width=100, height=50)
-        rightFrame.grid(row=0, column=1, padx=150, pady=10)
+        rightFrame.grid(row=0, column=1, padx=50, pady=10)
         listbox = Listbox(rightFrame, width=100, height=45)
         listbox.grid()
-        listbox.insert(END, "a list entry")
-        for item in ["one", "two", "three", "four"]:
-            listbox.insert(END, item)
+        listbox.insert(END, "DATE                           START FRAME                        END FRAME")
+        for i in range(0, 1000):
+            listbox.insert(END,"21.04.2018                           0                                1000")
+            listbox.insert(i)
+        scrollbar = Scrollbar(rightFrame)
+        scrollbar.grid(sticky="NSW",row=0, column=2,rowspan=2)
 
+        listbox.config(yscrollcommand=scrollbar.set)
+        scrollbar.config(command=listbox.yview)
 
 root = Tk()  # Makes the window
 root.geometry("1500x1200")
